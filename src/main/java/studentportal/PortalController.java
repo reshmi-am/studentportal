59ee6cf86841a942ddbcc26fc66501c85d1c9682
reshmi-am <36509012+reshmi-am@users.noreply.github.com>
2018-03-19 16:32:11 +0530

    code clean up

--- src/main/java/studentportal/PortalController.java
+++ src/main/java/studentportal/PortalController.java
@@ -2,8 +2,6 @@ package studentportal;
 
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,8 +23,6 @@ import studentportal.services.StudentService;
 @CrossOrigin(origins = "http://localhost:4200")
 public class PortalController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(AuthService.class);
-	
 	@Autowired
 	private StudentService service;
 	
@@ -35,6 +31,7 @@ public class PortalController {
 	
     @RequestMapping("/courses")
     public @ResponseBody List<CourseInfo> getCourses() {
+   
         return service.getAllCourses();
     }
     
